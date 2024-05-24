f9590f6b09422f985a154fc3a54e22072a4cf49f
Rubakumardirushan <dirusha-se19033@stu.kln.ac.lk>
2024-05-24 15:01:30 +0530

    while for

--- if.py
+++ if.py
@@ -4,3 +4,16 @@ if False:
 print("True")
 print("True")
 
+num=100
+i=0
+sum=0
+while i<num:
+    sum=sum+i
+    print(sum)
+    i=i+1
+
+num=[1,2,3,4,5,6,7,8,9,10]
+sum=0
+for i in num:
+    sum=sum+i
+    print(sum)
