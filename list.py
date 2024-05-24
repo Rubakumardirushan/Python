55c1fa56c8790e1a81c477ee2f26cd5dbb369989
Rubakumardirushan <dirusha-se19033@stu.kln.ac.lk>
2024-05-24 14:26:46 +0530

    tupels

---
+++ list.py
@@ -0,0 +1,13 @@
+# list data type#
+my_lis=[]
+lanaguge=['python','java','c','c++']
+
+   
+for range in lanaguge:
+    print(range)
+
+
+lanaguge[0]='php'
+
+for range in lanaguge:
+    print(range)
---
+++ tuple.py
@@ -0,0 +1,3 @@
+number=('one','two','three','four','five','one','two','three','four','five')
+print(number)
+ ## can't change the value of tuple
