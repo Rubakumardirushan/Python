668f39377f0ded88a2a2d49a197738a3389d8422
Rubakumardirushan <dirusha-se19033@stu.kln.ac.lk>
2024-05-24 16:46:50 +0530

    add

--- OOP/oopuser.py
+++ OOP/oopuser.py
@@ -1,7 +1,10 @@
 from user import user
 
 user1=user("user1","password1")
+user1=user("user1","password1")
+user1=user("user1","password1")
 
 print(user1.user_name)
 print(user1.user_password)
 
+print(user.user_count)
--- OOP/user.py
+++ OOP/user.py
@@ -1,7 +1,14 @@
 class user:
-    user_name= None
-    user_password= None
-   
-    def __init__(self,user_name,user_password):
-        self.user_name=user_name
-        self.user_password=user_password
+    user_count=0
+    def __init__(self, user_name, user_password):
+        self.user_name = user_name
+        self.user_password = user_password
+        user.user_count+=1
+
+    # Getter method for user_name
+    def get_user_name(self):
+        return self.__user_name
+
+    # Setter method for user_name
+    def set_user_name(self, user_name):
+        self.__user_name = user_name
