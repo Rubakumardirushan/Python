9cd3eb74f8a697f505ee9af05c57aa9dc61bcc2d
Rubakumardirushan <dirusha-se19033@stu.kln.ac.lk>
2024-12-15 18:23:48 +0530

    loops

--- Python-django/index.py
+++ Python-django/index.py
@@ -1,5 +1,5 @@
 print("hello world")
-
+"""
  ##1. Variables and Data Types
  #python
 #Copy code 
@@ -18,3 +18,32 @@ age= input("Enter your age: ")
 print("Your age is: ", age)
 name = input("Enter your name: ")
 print("Your name is: ", name)
+
+
+age_string="26"
+age_int=int(age_string)
+print(age_int)
+print(type(age_int))
+print(type(age_string))
+
+
+
+### if else 
+number=int(input("Enter a number: "))
+if number%2==0:
+    print(f"{number} is even")
+    if(number%4==0):
+            print(f"{number} is divisible by 4")
+    else:
+        print(f"{number} is not divisible by 4")
+else:
+    print(f"{number} is odd")
+
+
+"""
+##2. Loops
+# for loop
+for i in range(1, 5):
+     for j in range(1, 5):
+         print(j,end=" ")
+     print('')   
