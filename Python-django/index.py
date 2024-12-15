e472152dc5316b054945608b89edd292f7cafc91
Rubakumardirushan <dirusha-se19033@stu.kln.ac.lk>
2024-12-15 17:13:45 +0530

    python basic

--- Python-django/index.py
+++ Python-django/index.py
@@ -1,1 +1,20 @@
 print("hello world")
+
+ ##1. Variables and Data Types
+ #python
+#Copy code 
+# Variables and Data Types
+name = "Dirushan"         # String
+age = 22                   # Integer
+height = 5.9               # Float
+is_student = True          # Boolean
+
+print(name, age, height, is_student)
+
+
+##input output 
+
+age= input("Enter your age: ")
+print("Your age is: ", age)
+name = input("Enter your name: ")
+print("Your name is: ", name)
